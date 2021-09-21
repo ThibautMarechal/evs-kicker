53371ee0ab4882f9f01ad1d9a5b51c1af21ce038
Thibaut Maréchal <t.marechal@evs.com>
2021-09-21 11:55:46 +0200

    omit purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  purge: {
-    content: ['.src//pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    options: {
-      safelist: [/data-theme$/],
-    },
-  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
